2ce933291a5dd65a4e73a70f07d40530a3a4984b
Yair Dorantes <yairmasterlol@gmail.com>
2023-03-27 16:26:45 -0600

    jaja

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,11 +15,13 @@ const Form = () => {
   return (
     <div>
       <div className="w-full mx-auto mt-24 max-w-xs">
-        <div className="alert">Inicio de sesion de esclavo</div>
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="alert alert-success mb-10">
+          Inicio de sesion de esclavo 😀
+        </div>
+        <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-white text-sm font-bold mb-2"
               htmlFor="username"
             >
               Correo
@@ -34,7 +36,7 @@ const Form = () => {
           </div>
           <div className="mb-6">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-white text-sm font-bold mb-2"
               htmlFor="password"
             >
               Password
@@ -50,21 +52,21 @@ const Form = () => {
           <div className="flex items-center justify-between">
             <button
               onClick={sendLogin}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-blue-500 mx-auto hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
             >
               Ingresar
             </button>
-            <a
+            {/* <a
               className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
               href="#"
             >
               Forgot Password?
-            </a>
+            </a> */}
           </div>
         </form>
-        <p className="text-center text-gray-500 text-xs">
-          &copy;2020 Acme Corp. All rights reserved.
+        <p className="text-center text-white text-xs">
+          &copy;2023 Corporacion Bebote. Todos los derechos reservados.
         </p>
       </div>
     </div>
